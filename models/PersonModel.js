b42bc472fbec54380daa1d7de950496bfba1f1c3
Peter-JC-Wu <peterjcwu@gmail.com>
2023-06-22 11:00:04 -0400

    wrote schema for query

---
+++ models/PersonModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema ({
+  hair: { type: String, required: true},
+  eyes: { type: String, required: true},
+  weight: { type: Number, required: true},
+  height: { type: Number, required: true},
+  salary: { type: Number, required: true},
+  numKids: { type: Number, required: true},
+  kids: { type: Array, required: true},
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+module.exports = Person;
